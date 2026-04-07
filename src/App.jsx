dd35cf068df531121b61211d3b8a8234282ab4d5
Divansh <divanshsingh1612@gmail.com>
2026-04-07 15:55:43 +0530

    New folder created old one had error

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-sky-400">
+        Tailwind is Working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
